a4f199800c525691f7244b64c26c5bed5e8bf35b
Nathan Yee <nyee4@asu.edu>
2024-04-09 13:00:26 -0700

    added read in patient info functionality

--- GroupProject/src/application/Patient.java
+++ GroupProject/src/application/Patient.java
@@ -1,10 +1,6 @@
 package application;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
-
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -12,11 +8,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -29,8 +25,10 @@ public class Patient extends Application {
 	private Stage primaryStage;
 	private GridPane grid;
 	private Users.User user;
-    private Label messageLabel, contactLabel, insuranceLabel, pharmacyLabel, summaryLabel, welcome;
-    Button send, generate, changeInfo;
+    private Label messageLabel, contactLabel, insuranceLabel, pharmacyLabel, summaryLabel, welcome,
+    		phoneLabel, emailLabel, insuranceProviderLabel, insurancePolicyNumLabel, pharmacyNameLabel, 
+    		pharmacyAddressLabel, emptyLabel;
+    Button send, changeInfo;
 
 	
 	 public Patient(Users.User user) { this.user = user; }
@@ -60,15 +58,45 @@ public class Patient extends Application {
         contactLabel.setTextFill(Color.WHITE);
         contactLabel.setAlignment(Pos.CENTER);
         
+        phoneLabel = new Label("Phone Number:");
+        phoneLabel.setFont(Font.font("Arial", 12));
+        phoneLabel.setTextFill(Color.WHITE);
+        phoneLabel.setAlignment(Pos.CENTER);
+        
+        emailLabel = new Label("Email:");
+        emailLabel.setFont(Font.font("Arial", 12));
+        emailLabel.setTextFill(Color.WHITE);
+        emailLabel.setAlignment(Pos.CENTER);
+        
         insuranceLabel = new Label("Insurance Information");
         insuranceLabel.setFont(Font.font("Arial", 20));
         insuranceLabel.setTextFill(Color.WHITE);
         insuranceLabel.setAlignment(Pos.CENTER);
         
+        insuranceProviderLabel = new Label("Insurance Provider:");
+        insuranceProviderLabel.setFont(Font.font("Arial", 12));
+        insuranceProviderLabel.setTextFill(Color.WHITE);
+        insuranceProviderLabel.setAlignment(Pos.CENTER);
+        
+        insurancePolicyNumLabel = new Label("Policy Number:");
+        insurancePolicyNumLabel.setFont(Font.font("Arial", 12));
+        insurancePolicyNumLabel.setTextFill(Color.WHITE);
+        insurancePolicyNumLabel.setAlignment(Pos.CENTER);
+        
         pharmacyLabel = new Label("Pharmacy Information");
         pharmacyLabel.setFont(Font.font("Arial", 20));
         pharmacyLabel.setTextFill(Color.WHITE);
         pharmacyLabel.setAlignment(Pos.CENTER);
+        
+        pharmacyNameLabel = new Label("Pharmacy Name:");
+        pharmacyNameLabel.setFont(Font.font("Arial", 12));
+        pharmacyNameLabel.setTextFill(Color.WHITE);
+        pharmacyNameLabel.setAlignment(Pos.CENTER);
+        
+        pharmacyAddressLabel = new Label("Address:");
+        pharmacyAddressLabel.setFont(Font.font("Arial", 12));
+        pharmacyAddressLabel.setTextFill(Color.WHITE);
+        pharmacyAddressLabel.setAlignment(Pos.CENTER);
         
         messageLabel = new Label("Message Your Provider Here");
         messageLabel.setFont(Font.font("Arial", 20));
@@ -80,33 +108,42 @@ public class Patient extends Application {
         summaryLabel.setTextFill(Color.WHITE);
         summaryLabel.setAlignment(Pos.CENTER);
         
+        emptyLabel = new Label("One or more fields is empty");
+        emptyLabel.setFont(Font.font("Arial", 12));
+        
         grid.add(contactLabel, 0, 0);
         grid.add(messageLabel, 1, 0);
-        grid.add(summaryLabel, 2, 0);
         
-        TextArea contacts = new TextArea();
-        contacts.setPrefWidth(100);
-        contacts.setPrefHeight(100);
-        contacts.setPrefRowCount(10);
-        contacts.setWrapText(true);
-        TextArea insurance = new TextArea();
-        insurance.setPrefWidth(100);
-        insurance.setPrefHeight(100);
-        insurance.setPrefRowCount(10);
-        TextArea pharmacy = new TextArea();
-        contacts.setWrapText(true);
-        pharmacy.setPrefWidth(100);
-        pharmacy.setPrefHeight(100);
-        pharmacy.setPrefRowCount(10);
-        pharmacy.setWrapText(true);
+        TextField phone = new TextField();
+        TextField email = new TextField();
+        TextField insuranceProvider = new TextField();
+        TextField insurancePolicyNum = new TextField();
+        TextField pharmacyName = new TextField();
+        TextField pharmacyAddress = new TextField();
+        pharmacyAddress.setPromptText("Address Line 1");
+        TextField pharmacyCity = new TextField();
+        pharmacyCity.setPromptText("City");
+        pharmacyCity.setMaxWidth(95);
+        TextField pharmacyState = new TextField();
+        pharmacyState.setPromptText("State");
+        pharmacyState.setMaxWidth(45);
+        TextField pharmacyZip = new TextField();
+        pharmacyZip.setPromptText("ZIP");
+        pharmacyZip.setMaxWidth(50);
         
-        VBox patientInfo = new VBox(10);
-        patientInfo.getChildren().addAll(contacts, insuranceLabel, insurance, pharmacyLabel, pharmacy);
+        HBox cityStateZip = new HBox(5);
+        cityStateZip.getChildren().addAll(pharmacyCity, pharmacyState, pharmacyZip);
+        
+        VBox patientInfo = new VBox(5);
+        patientInfo.getChildren().addAll(phoneLabel, phone, emailLabel, email, insuranceLabel, 
+        		insuranceProviderLabel, insuranceProvider, insurancePolicyNumLabel, insurancePolicyNum, 
+        		pharmacyLabel, pharmacyNameLabel, pharmacyName, pharmacyAddressLabel, pharmacyAddress,
+        		cityStateZip);
         grid.add(patientInfo, 0, 1, 1, 1);
         
         TextArea textMessage = new TextArea();
-        textMessage.setPrefWidth(100);
-        textMessage.setPrefHeight(300);
+        textMessage.setPrefWidth(400);
+        textMessage.setPrefHeight(170);
         textMessage.setPrefRowCount(10);
         textMessage.setWrapText(true);
         grid.add(textMessage, 1, 1, 1, 1);
@@ -118,7 +155,8 @@ public class Patient extends Application {
         {
         	 while( scan.hasNextLine()) {
              	String line = scan.nextLine();
-             	if(line.contains(user.getName())&& line.contains(user.getId()))
+             	String[] splitLine = line.split(":");
+       		 	if(splitLine[0].equals(user.getName()))
   //i know this looks dumb but its so we dont forget to talk about it.   			
              			//&& line.contains(user.getId())
              			
@@ -133,11 +171,11 @@ public class Patient extends Application {
         
        
         TextArea summary = new TextArea();
-        summary.setPrefWidth(100);
-        summary.setPrefHeight(300);
+        summary.setPrefWidth(400);
+        summary.setPrefHeight(170);
         summary.setPrefRowCount(10);
         summary.setWrapText(true);
-        grid.add(summary, 2, 1, 1, 1);
+        summary.setEditable(false);
         
        // visit summary read
         File visits = new File("./visits.txt");
@@ -147,7 +185,8 @@ public class Patient extends Application {
         	 while( scan.hasNextLine()) 
         	 {
         		 String line = scan.nextLine();
-        		 if(line.contains(user.getName())&& line.contains(user.getId()))
+        		 String[] splitLine = line.split(",");
+        		 if(splitLine[0].equals(user.getName()))
              	 {
              		 summary.appendText("\n" + line + "\n");
              	 }
@@ -158,43 +197,97 @@ public class Patient extends Application {
         }
         
         send = new Button("Send Message");
-        grid.add(send, 1, 2);
-
-        generate = new Button("Generate Summary");
-        grid.add(generate, 2, 2);
-
         changeInfo = new Button("Update Info");
-        grid.add(changeInfo, 0, 2);
+        patientInfo.getChildren().add(changeInfo);
+        
+        VBox secondColumn = new VBox(5);
+        secondColumn.getChildren().addAll(textMessage, send, summaryLabel, summary);
+        grid.add(secondColumn, 1, 1);
+        
+        try {
+    		BufferedReader br = new BufferedReader(new FileReader("patientInfo.txt"));
+    		String line;
+    		while((line = br.readLine()) != null) {
+    			String[] splitLine = line.split("\\|");
+    			if (splitLine[0].equals(user.getName())) {
+    				phone.setText(splitLine[1]);
+    				email.setText(splitLine[2]);
+    				insuranceProvider.setText(splitLine[3]);
+    				insurancePolicyNum.setText(splitLine[4]);
+    				pharmacyName.setText(splitLine[5]);
+    				pharmacyAddress.setText(splitLine[6]);
+    				pharmacyCity.setText(splitLine[7]);
+    				pharmacyState.setText(splitLine[8]);
+    				pharmacyZip.setText(splitLine[9]);
+    			}
+    		}
+    		br.close();
+    	}
+    	catch (IOException ioe) {
+    		System.out.println("Failed to fetch patient info");
+    	}
         
         changeInfo.setOnAction(e -> {
-        	try {
-        		BufferedWriter bw = new BufferedWriter(new FileWriter("patientInfo.txt"));
-        		BufferedReader br = new BufferedReader(new FileReader("patientInfo.txt"));
-        		String lines = "";
-        		String line;
-        		boolean hasEntry = false;
-        		while((line = br.readLine()) != null) {
-        			String[] splitLine = line.split(",");
-        			if (splitLine[0] == user.getName()) {
-        				splitLine[1] = contacts.getText();
-        				splitLine[2] = insurance.getText();
-        				splitLine[3] = pharmacy.getText();
-        				hasEntry = true;
-        			}
-        			else {
-        				lines += line;
-        			}
+        	boolean hasEmpty = false;
+        	TextField[] fields = {phone, email, insuranceProvider, insurancePolicyNum, pharmacyName,
+        			pharmacyAddress, pharmacyCity, pharmacyState, pharmacyZip};
+        	for (TextField tf : fields) {
+        		if (tf.getText().isBlank()) {
+        			hasEmpty = true;
+        			break;
         		}
-        		if (!hasEntry) {
-        			lines += user.getName() + "," + contacts.getText() + "," + insurance.getText() + ","
-        					+ pharmacy.getText();
-        		}
-        		bw.write(lines);
-        		br.close();
-        		bw.close();
         	}
-        	catch (IOException ioe) {
-        		System.out.println("Failed to update patient info");
+        	if (!hasEmpty) {
+	        	try {
+	        		BufferedWriter bw = new BufferedWriter(new FileWriter("patientInfo.txt"));
+	        		BufferedReader br = new BufferedReader(new FileReader("patientInfo.txt"));
+	        		String lines = "";
+	        		String line;
+	        		boolean hasEntry = false;
+	        		while((line = br.readLine()) != null) {
+	        			String[] splitLine = line.split("\\|");
+	        			if (splitLine[0] == user.getName()) {
+	        				splitLine[1] = phone.getText();
+	        				splitLine[2] = email.getText();
+	        				splitLine[3] = insuranceProvider.getText();
+	        				splitLine[4] = insurancePolicyNum.getText();
+	        				splitLine[5] = pharmacyName.getText();
+	        				splitLine[6] = pharmacyAddress.getText();
+	        				splitLine[7] = pharmacyCity.getText();
+	        				splitLine[8] = pharmacyState.getText();
+	        				splitLine[9] = pharmacyZip.getText();
+	        				hasEntry = true;
+	        			}
+	        			else {
+	        				lines += line;
+	        			}
+	        		}
+	        		if (!hasEntry) {
+	        			lines += user.getName() + "|" + phone.getText() + "|" + email.getText() + "|"
+	        					+ insuranceProvider.getText() + "|" + insurancePolicyNum.getText() + "|" 
+	        					+ pharmacyName.getText() + "|" + pharmacyAddress.getText() + "|"
+	        					+ pharmacyCity.getText() + "|" + pharmacyState.getText() + "|"
+	        					+ pharmacyZip.getText();
+	        		}
+	        		bw.write(lines);
+	        		br.close();
+	        		bw.close();
+	        		emptyLabel.setText("Patient info updated successfully");
+	        		emptyLabel.setTextFill(Color.BLACK);
+	           		if (GridPane.getColumnIndex(emptyLabel) == null) {
+	        			grid.add(emptyLabel, 0, 2);
+	        		}
+	        	}
+	        	catch (IOException ioe) {
+	        		System.out.println("Failed to update patient info");
+	        	}
+        	}
+        	else {
+        		emptyLabel.setText("One or more fields is empty");
+    			emptyLabel.setTextFill(Color.RED);
+        		if (GridPane.getColumnIndex(emptyLabel) == null) {
+        			grid.add(emptyLabel, 0, 2);
+        		}
         	}
         });
 
@@ -206,7 +299,7 @@ public class Patient extends Application {
         // You can access other user information using the user object here
         borderPane.setLeft(grid);
         borderPane.setTop(welcome);
-        borderPane.setAlignment(welcome, javafx.geometry.Pos.CENTER);
+        BorderPane.setAlignment(welcome, javafx.geometry.Pos.CENTER);
         Scene scene = new Scene(borderPane, 700, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
